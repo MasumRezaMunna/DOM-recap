2822914b7dcc3dc0066c6c32643d8d2c7c7437fe
MasumRezaMunna <masumrezamunna2005@gmail.com>
2026-01-02 00:00:25 +0600

    added li using js

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 function ClickMe() {
     document.getElementById("demo").innerHTML = Date();
 }
+
+// let div = document.createElement("div");
+// div.innerHTML = "<p>Hello, this is a new div!</p>";
+// document.body.appendChild(div);
+
+// let div = document.createElement("div");
+// div.className = "my-class";
+// div.id = "my-id";
+
+// let h1 = document.createElement("h1");
+// h1.textContent = "Hello, World!";
+// div.appendChild(h1);
+
+// document.body.appendChild(div);
+
+let ul = document.createElement("ul");
+ul.id = "menu";
+
+let li1 = document.createElement("li");
+li1.textContent = "Home";
+ul.appendChild(li1);
+
+document.body.appendChild(ul);
